3926e2b32be8f315f78b631955fa86ebf814dc1a
sisthema-admin <149604398+sisthema-admin@users.noreply.github.com>
2023-11-02 11:04:14 +0100

    feat: fix bug prisma

--- service/auth.validate.js
+++ service/auth.validate.js
@@ -12,22 +12,25 @@ const configAuth = {
             name: "Credentials",
             async authorize(credentials) {
                 //Check if the user exists.
-
-                if (!isValidEmail(credentials.email))
-                    throw new Error("E-mail non valida!!")
-
-                const user = await prisma.user.findUnique({ where: { email: credentials.email } })
-                if (!user) {
-                    throw new Error("Utente non trovato!!")
-                } else {
-                    const isPasswordCorrect = await compare(credentials.password, user.password)
-                    if (!isPasswordCorrect) {
-                        throw new Error("Credenziali errate!")
+                try {
+                    if (!isValidEmail(credentials.email))
+                        throw new Error("E-mail non valida!!")
+                    // const user = await prisma.user.findFirst({ where: { email: credentials.email } })
+                    const user = await prisma.user.findUnique({ where: { email: credentials.email } })
+                    if (!user) {
+                        throw new Error("Utente non trovato!!")
                     } else {
-                        // everything ok!!
-                        delete user.password
-                        return user
+                        const isPasswordCorrect = await compare(credentials.password, user.password)
+                        if (!isPasswordCorrect) {
+                            throw new Error("Credenziali errate!")
+                        } else {
+                            // everything ok!!
+                            delete user.password
+                            return user
+                        }
                     }
+                } catch (error) {
+                    console.log(error)
                 }
             },
         }),
